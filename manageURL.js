d7c6f295ee289b63ed43970c9da2fa5cbfe67f19
frdrcpeter007 <frdrcpeter@gmail.com>
2019-10-05 16:01:50 +0200

    Liaison à l'API en ligne

--- manageURL.js
+++ manageURL.js
@@ -1,5 +1,7 @@
 module.exports.URL = function () {
     return{
-        "API": "http://localhost:3333"
+        //https://immo-jach-api.herokuapp.com
+        //http://localhost:3333
+        "API": "https://immo-jach-api.herokuapp.com"
     }
 }
