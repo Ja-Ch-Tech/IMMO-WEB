9a092e86f3cf38e118a2b30c78699ac519d1b1f4
josuembuyu <josuembuyu94@gmail.com>
2019-10-07 12:42:15 +0100

    Propriete

--- routes/biens.js
+++ routes/biens.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* DETAILS D'UN BIEN */
-router.get('/:bien_id/informations', function(req, res, next) {
+router.get('/:bien_id/:type_/informations', function(req, res, next) {
   res.render('profile/informations', { 
   	title: 'Profile' 
   });
