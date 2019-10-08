13a3c52efa37a123bf5cbdcfbb88ca6b144a4a63
josuembuyu <josuembuyu94@gmail.com>
2019-10-08 17:05:06 +0100

    Interfaces du profil

--- routes/profile.js
+++ routes/profile.js
@@ -6,4 +6,31 @@ router.get('/:user_id/informations', function(req, res, next) {
   res.render('profile/informations', { title: 'Profile' });
 });
 
+/* EXECUTE LA PAGE PHOTO PROFIL USER */
+router.get('/:user_id/photo', function (req, res, next) {
+	res.render('profile/photo', {
+		title : 'Photo de profile'
+	})
+})
+
+/* EXECUTE LA PAGE CONFIGURATION DU COMPTE  */
+router.get('/:user_id/securite', function (req, res, next) {
+	res.render('profile/securite', {
+		title : 'Configuration du compte'
+	})
+})
+
+/* EXECUTE LA PAGE RENVOYANT LES BIENS D'UN BAYEUR OU VENDEUR */
+router.get('/:user_id/biens', function (req, res, next) {
+	res.render('profile/biens', {
+		title : 'Vos biens'
+	})
+})
+
+/* EXECUTE LA PAGE RENVOYANT LE FORMULAIRE D'AJOUR D'UN BIEN */
+router.get('/:user_id/biens/ajouter', function (req, res, next) {
+	res.render('profile/ajoutBien', {
+		title : 'Publiez un bien'
+	})
+})
 module.exports = router;
