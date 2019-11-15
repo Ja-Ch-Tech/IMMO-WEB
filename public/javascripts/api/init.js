c10082f4e0689278b68655ca843cf51052ecced2
frdrcpeter007 <frdrcpeter@gmail.com>
2019-11-15 18:05:29 +0100

    Fixation des liens

--- public/javascripts/api/init.js
+++ public/javascripts/api/init.js
@@ -1,6 +1,6 @@
 function getHostAPI() {
-    return "http://localhost:3333";
-    //return "https://immo-jach-api.herokuapp.com";
+    //return "http://localhost:3333";
+    return "https://immo-jach-api.herokuapp.com";
 }
 /**
  * Fonction permettant de récupérer l'identifiant de l'utilisateur en session
