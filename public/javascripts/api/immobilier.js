c3ce3a3cc16a96b94f5dbe92d5d0a13330e25630
Jacoblimbi <jacob@jachtechnologies.com>
2020-09-24 14:28:22 +0100

    correction textes

--- public/javascripts/api/immobilier.js
+++ public/javascripts/api/immobilier.js
@@ -416,7 +416,7 @@ function getStatType() {
                     var contentHead = `<div class="row" id="elementProp">
                                     <div class="col-12">
                                         <div class="section-heading wow fadeInUp" data-wow-delay="200ms">
-                                            <h2>Trouvez par <span>type d'immobilier</span></h2>
+                                            <h2><span>Types d'immobiliers</span></h2>
                                             <p></p>
                                         </div>
                                     </div>
